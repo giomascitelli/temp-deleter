a2d3fef090d0c7a7e35230e61312b1683270c648
Giovanni Mascitelli <47045018+giomascitelli@users.noreply.github.com>
2024-03-26 11:54:19 -0300

    Update temp-deleter.py
    The code that should have been commented is commented properly now.

--- temp-deleter.py
+++ temp-deleter.py
@@ -14,17 +14,17 @@ temp_dirs = [
     os.path.join(user_profile, r"AppData\Local\Microsoft\Windows\Explorer")
 ]
 
-# Chaves de registro para arquivos temporários da Internet Explorer
-ie_temp_keys = [
-    r"HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\History\Current",
-    r"HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\History\Visited",
-]
+# # Chaves de registro para arquivos temporários do Internet Explorer
+# ie_temp_keys = [
+#     r"HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\History\Current",
+#     r"HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\History\Visited",
+# ]
 
-# Chaves de registro para arquivos temporários do Windows Defender
-wd_temp_keys = [
-    r"HKLM\SOFTWARE\Microsoft\Windows Defender\Settings\IntelligentOfflineScan\History",
-    r"HKLM\SOFTWARE\Microsoft\Windows Defender\Settings\MpEngine\Signatures",
-]
+# # Chaves de registro para arquivos temporários do Windows Defender
+# wd_temp_keys = [
+#     r"HKLM\SOFTWARE\Microsoft\Windows Defender\Settings\IntelligentOfflineScan\History",
+#     r"HKLM\SOFTWARE\Microsoft\Windows Defender\Settings\MpEngine\Signatures",
+# ]
 
 def limpar_diretorio(diretorio):
     arquivos_deletados = []
@@ -44,25 +44,25 @@ def limpar_diretorio(diretorio):
 
     return arquivos_deletados
 
-def limpar_registro(chave, valor):
-    try:
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, chave, 0, winreg.KEY_WRITE) as k:
-            winreg.DeleteValue(k, valor)
-    except PermissionError:
-        print(f"Erro ao excluir {valor} de {chave}: permissão negada")
-    except FileNotFoundError:
-        pass
+# def limpar_registro(chave, valor):
+#     try:
+#         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, chave, 0, winreg.KEY_WRITE) as k:
+#             winreg.DeleteValue(k, valor)
+#     except PermissionError:
+#         print(f"Erro ao excluir {valor} de {chave}: permissão negada")
+#     except FileNotFoundError:
+#         pass
 
 def limpar_temp():
     arquivos_deletados = []
     for diretorio in temp_dirs:
         arquivos_deletados.extend(limpar_diretorio(diretorio))
 
-    for chave in ie_temp_keys:
-        limpar_registro(chave, "Visited")
+    # for chave in ie_temp_keys:
+    #     limpar_registro(chave, "Visited")
 
-    for chave in wd_temp_keys:
-        limpar_registro(chave, "ScanHistory")
+    # for chave in wd_temp_keys:
+    #     limpar_registro(chave, "ScanHistory")
 
     return arquivos_deletados
 
